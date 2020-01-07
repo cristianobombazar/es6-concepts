dd34dcbc7216e923602f6d9dc3fb808f0e1a42d5
Cristiano Bombazar <cristiaano.bombazar@gmail.com>
2020-01-07 20:50:12 -0300

    Array operations.

--- buldle.js
+++ buldle.js
@@ -1,26 +1,19 @@
 "use strict";
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-var Math =
-/*#__PURE__*/
-function () {
-  function Math() {
-    _classCallCheck(this, Math);
-  }
-
-  _createClass(Math, null, [{
-    key: "sum",
-    value: function sum(a, b) {
-      return a + b;
-    }
-  }]);
-
-  return Math;
-}();
-
-console.log(Math.sum(1, 2));
+var arr = [1, 3, 4, 5, 8, 9, 10];
+var result = arr.map(function (item, index) {
+  return item * index;
+});
+console.log(result);
+var reduced = arr.reduce(function (total, next) {
+  return total + next;
+});
+console.log(reduced);
+var filtered = arr.filter(function (item) {
+  return item % 2 === 0;
+});
+console.log(filtered);
+var find = arr.filter(function (item) {
+  return item === 2;
+});
+console.log(find);
--- main.js
+++ main.js
@@ -1,6 +1,20 @@
-class Math {
-    static sum(a, b) {
-        return a +b;
-    }
-}
-console.log(Math.sum(1, 2));
+const arr = [1, 3 ,4, 5, 8, 9, 10];
+const result = arr.map(function (item, index) {
+    return item * index;
+});
+console.log(result);
+
+const reduced = arr.reduce(function (total, next) {
+    return total + next;
+});
+console.log(reduced);
+
+const filtered = arr.filter(function (item) {
+    return item % 2 === 0;
+});
+console.log(filtered);
+
+const find = arr.filter(function (item) {
+    return item === 2;
+});
+console.log(find);
