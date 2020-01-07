4730d84730bc5d758cf6682663954ddf48785b10
Cristiano Bombazar <cristiaano.bombazar@gmail.com>
2020-01-07 20:39:10 -0300

    static methods.

--- main.js
+++ main.js
@@ -1,29 +1,6 @@
-class List {
-    constructor() {
-        this.data = [];
-    }
-
-    add(data) {
-        this.data.push(data);
-        console.log(this.data);
+class Math {
+    static sum(a, b) {
+        return a +b;
     }
 }
-
-class TodoList extends List{
-
-    constructor() {
-        super();
-        this.user = "Cristiano Bombazar"
-    }
-
-    showUser() {
-        console.log(this.user);
-    }
-}
-
-
-var myList = new TodoList();
-document.getElementById('newTodo').onclick = function () {
-    myList.add('new Todo')
-}
-myList.showUser();
+console.log(Math.sum(1, 2));
