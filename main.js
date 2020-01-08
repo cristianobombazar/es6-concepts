4922053ee969660c0ee3b6e03c077442e6d7cb3d
Cristiano Bombazar <cristiaano.bombazar@gmail.com>
2020-01-07 22:09:39 -0300

    Webpack dev server (live reload)

--- main.js
+++
@@ -1,9 +0,0 @@
-import defaultSum, {sum as name, sub} from './functions';
-import sumFromFile from './sum';
-import * as functions from './functions';
-
-console.log(name(1, 3));
-console.log(defaultSum(1, 3));
-console.log(sub(3, 1));
-console.log(sumFromFile(3, 1));
-console.log(functions.sub(1, 3));
