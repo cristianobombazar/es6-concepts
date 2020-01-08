02b0bb1b00603dc45dc47d39ebba9ddfbc9b1e25
Cristiano Bombazar <cristiaano.bombazar@gmail.com>
2020-01-08 20:04:27 -0300

    Rendering html.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ class App {
     constructor() {
         this.repositories = [];
         this.formEl = document.getElementById('repo-form');
+        this.listEl = document.getElementById('repo-list');
         this.registerHandlers();
     }
 
@@ -18,7 +19,34 @@ class App {
             urlPath: 'https://avatars.githubusercontent.com/u/28929274?v=4',
             htmlPath: 'https://github.com/cristianobombazar'
         });
-        console.log(this.repositories);
+        this.render();
+    }
+
+    render() {
+        this.listEl.innerHTML = '';
+        this.repositories.forEach(repository => {
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repository.urlPath);
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repository.name));
+
+            let descriptionEl = document.createElement('p');
+            descriptionEl.appendChild(document.createTextNode(repository.description));
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target', '_blank');
+            linkEl.appendChild(document.createTextNode('Acessar'));
+
+            let listItemEl = document.createElement('li');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descriptionEl);
+            listItemEl.appendChild(linkEl);
+
+            this.listEl.appendChild(listItemEl);
+
+        });
     }
 }
 
